b563fcbca5351c481b481e4e239f626b95bf7f98
Carlos <cdzamora02@outlook.es>
2023-08-06 15:26:38 -0600

    feat(ownfraction): add gcd() method

--- notes/posts/fracciones/ownfraction.py
+++ notes/posts/fracciones/ownfraction.py
@@ -7,6 +7,21 @@ class MyFraction:
     def __str__(self) -> str:
         return f"{self.numerator}/{self.denominator}"
     
+    @staticmethod
+    def gcd(a:int, b:int) -> int:
+        
+        if a<b:
+            a,b = b,a
+        
+        r = a % b
+        while r != 0:
+            
+            a = b
+            b = r
+            r = a % b
+
+        return b
+    
 
 if __name__ == "__main__":
     
@@ -14,3 +29,6 @@ if __name__ == "__main__":
     q2 = MyFraction(3.14, 2.72)
     print(q1)
     print(q2)
+
+    r = MyFraction.gcd(220,140)
+    print(r)
