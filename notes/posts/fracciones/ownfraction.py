fe3468559f4b9a7c135e9c9c6a8d27d06481cb27
Carlos <cdzamora02@outlook.es>
2023-08-11 10:46:55 -0600

    feat(ownfraction): add simplify() method and change __init__() method for integers

--- notes/posts/fracciones/ownfraction.py
+++ notes/posts/fracciones/ownfraction.py
@@ -3,10 +3,27 @@ class MyFraction:
     def __init__(self, arg1, arg2 = 1) -> None:
         
         if isinstance(arg1, int) and isinstance(arg2, int):
-            d = self.gcd(arg1, arg2) 
-            self.numerator = arg1//d
-            self.denominator = arg2//d
+            if arg2 == 0:
+                raise ZeroDivisionError("the denominator should be non-zero") 
+            elif arg1 == 0:
+                self.numerator = arg1
+                self.denominator = 1
+            else:
+                self.numerator = arg1
+                self.denominator = arg2
+                self.simplify()
+
+    def simplify(self):
         
+        abs_n = abs(self.numerator)
+        abs_d = abs(self.denominator)
+        d = self.gcd(abs_n,abs_d)
+        self.numerator //= d
+        self.denominator //= d
+
+        if self.denominator < 0:
+            self.numerator *= -1
+            self.denominator *= -1
 
     def __str__(self) -> str:
         return f"{self.numerator}/{self.denominator}"
@@ -29,7 +46,7 @@ class MyFraction:
 
 if __name__ == "__main__":
     
-    q1 = MyFraction(6, 3)
-    q2 = MyFraction(3, 6)
+    q1 = MyFraction(-6, -4)
+    q2 = MyFraction(0, 6)
     print(q1)
     print(q2)
